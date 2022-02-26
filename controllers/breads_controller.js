9602b8b3bc6946c670a4f41f98bebfe36f339193
PurpleHurt <91793323+PurpleHurt@users.noreply.github.com>
2022-02-26 14:04:51 -0600

    create and new routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -10,16 +10,33 @@ breads.get('/', (req, res) => {
     })
 })
 
-module.exports = breads
+// NEW
+breads.get('/new', (req, res) => {
+    res.render('new')
+})
 
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     //res.send(Bread[req.params.arrayIndex])
     if (Bread[req.params.arrayIndex]) {
-        res.render('Show', {
+        res.render('show', {
             bread: Bread[req.params.arrayIndex]
         })
     } else {
-        res.send('404')
+        res.render('NotFound')
     }
 })
+
+// CREATE
+breads.post('/', (req, res) => {
+    console.log(req.body)
+    if (req.body.hasGluten === 'on') {
+        req.body.hasGluten === 'true'
+    } else {
+        req.body.hasGlutten === 'false'
+    }
+    Bread.push(req.body)
+    res.redirect('/breads')
+})
+
+module.exports = breads
